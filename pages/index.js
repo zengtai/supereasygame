61f2c60f7a71864ba6471c6356dea0d4f0d4411a
Tai Zeng <zengtai@gmail.com>
2022-04-05 09:45:09 +0800

    修复一些小问题

--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,16 @@ import {
   starIcon,
 } from "../components/Icons";
 import { getGames, getCategories } from "../lib/api";
-import Link from "next/link";
-import Image from "../components/Image";
 import Layout from "../components/Layout";
 import { SITE_NAME, HOME_ADS_ID } from "../lib/constants";
 import List from "../components/List";
 import Adsense from "../components/Adsense";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Category from "../components/Category";
+import ListItem from "../components/ListItem";
 
 export default function Home({ games, newGames, featuredGames, categories }) {
-  const initGames = games.slice(0, 24);
+  const initGames = games.slice(0, 48);
   const total = games.length;
 
   const [scrollGames, setScrollGames] = useState(initGames);
@@ -58,34 +57,10 @@ export default function Home({ games, newGames, featuredGames, categories }) {
               loader={<div className="my-2 text-center">Loading...</div>}
             >
               <ul className="grid grid-cols-4 gap-3 py-3 md:grid-cols-6 md:gap-6 xl:grid-cols-8 2xl:grid-cols-12">
-                {scrollGames.map((game) => (
-                  <li
-                    key={game.id}
-                    className="second:col-span-2 md:second:col-auto second:row-span-2 md:second:row-auto"
-                  >
-                    <Link href={`/game/${game.slug}`}>
-                      <a className="group md:delay-50 duration-400 relative block aspect-square overflow-hidden rounded-2xl shadow-md shadow-black/30 transition ease-in-out hover:shadow-lg hover:shadow-black/40 md:hover:origin-bottom md:hover:scale-110">
-                        <Image
-                          src={game.icon}
-                          alt={game.title}
-                          width={200}
-                          height={200}
-                          layout="responsive"
-                          className="bg-loading w-full bg-center bg-no-repeat"
-                        />
-                        <div className="absolute -bottom-[150%] hidden h-full w-full items-end justify-center text-center text-xs font-semibold group-hover:bg-gradient-to-t group-hover:from-black group-hover:to-black/0 sm:flex md:group-hover:bottom-0">
-                          <div className="h-auto w-full text-ellipsis p-2 text-center">
-                            <h3 className="leading-4">{game.title}</h3>
-                            <p className="flex flex-row items-center justify-center text-xl font-bold text-orange-500">
-                              {starIcon()}
-                              {game.stars}
-                            </p>
-                          </div>
-                        </div>
-                      </a>
-                    </Link>
-                  </li>
-                ))}
+                <ListItem
+                  games={scrollGames}
+                  className={`second:col-span-2 md:second:col-auto second:row-span-2 md:second:row-auto`}
+                />
               </ul>
             </InfiniteScroll>
           </div>
