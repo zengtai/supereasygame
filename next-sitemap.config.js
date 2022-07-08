e756228f01262c7052f88d20dd37b6ef82fc1284
zengtai <zengtai@gmail.com>
2022-07-08 14:59:47 +0800

    添加sitemap插件

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://www.supereasygame.com",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
+
+// export default config;
