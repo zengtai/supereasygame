596c396457af549cacacb988de12ed1b0cdd1b50
Tai Zeng <zengtai@gmail.com>
2022-11-24 11:04:15 +0800

    设置生产环境移除log

--- next.config.js
+++ next.config.js
@@ -2,11 +2,13 @@ module.exports = {
   images: {
     unoptimized: true,
     // disableStaticImages: true,
-
   },
   swcMinify: true,
   reactStrictMode: true,
   // distDir: "build",
   trailingSlash: true,
   // basePath: "/",
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "development" ? false : true,
+  },
 };
