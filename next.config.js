482665dede7b91ecfa6bca2e703622146d1aad41
Tai Zeng <zengtai@gmail.com>
2022-12-05 15:08:17 +0800

    修改buildid

--- next.config.js
+++ next.config.js
@@ -13,6 +13,6 @@ module.exports = {
   },
   generateBuildId: async () => {
     // You can, for example, get the latest git commit hash here
-    return "20221125";
+    return "20221202";
   },
 };
